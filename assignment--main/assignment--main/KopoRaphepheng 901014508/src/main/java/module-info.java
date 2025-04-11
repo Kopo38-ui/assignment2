350938b29cf785188269bde03f72978bb2fa9851
Kopo38-ui <raphephengkopo38@gmail.com>
2025-04-11 15:10:23 +0200

    Delete assignment--main/assignment--main/KopoRaphepheng 901014508 directory

--- assignment--main/assignment--main/KopoRaphepheng 901014508/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.koporaphepheng {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-    requires javafx.swing;
-    opens com.example.koporaphepheng to javafx.fxml;
-    exports com.example.koporaphepheng;
-}
